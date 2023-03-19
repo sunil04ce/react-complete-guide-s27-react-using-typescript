22cd7e63a19c1d0a861b98a87bed2d235b9a2b4d
sunil parmar <sunil04ce@gmail.com>
2023-03-19 18:51:08 +0530

    add,remove todo item using typescript, display in list

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,9 +1,30 @@
 import "./App.css";
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
+import { useState } from "react";
 
 function App() {
+  // const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevState) => {
+      return prevState.concat(newTodo);
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevState) => {
+      return prevState.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div>
-      <h1>Hello !</h1>
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
 }
